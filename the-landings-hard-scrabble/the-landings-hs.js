96aff2f4bf2ed273fea89590aa0356388b241fbb
leahjmoz <leahjmoz@gmail.com>
2025-11-25 13:06:09 -0500

    Landings page done, link to unfinished about me page

--- the-landings-hard-scrabble/the-landings-hs.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // Add the fade-in class after a short delay to ensure CSS is applied
-  setTimeout(() => {
-    document.body.classList.add('fade-in');
-  }, 100); // Adjust delay as needed
-});
---
+++ the-landings-hs/the-landings-hs.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Add the fade-in class after a short delay to ensure CSS is applied
+  setTimeout(() => {
+    document.body.classList.add('fade-in');
+  }, 100); // Adjust delay as needed
+});
